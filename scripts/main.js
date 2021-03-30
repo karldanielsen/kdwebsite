8b6159a6139daa09f153b6a7e1fd04be887fbb52
karldanielsen <karlpantry@gmail.com>
2021-03-29 21:18:16 -0700

    IMP starfall

--- scripts/main.js
+++ scripts/main.js
@@ -5,4 +5,28 @@ $(document).ready(function() {
             "padding-top": "2px"
         }, 200, function() {});
     });
+    
+    $(".star").each(function(i,obj) {
+        setInterval(function() {
+            starFall(obj);
+        }, 2300+i*600);
+    });
 });
+
+
+function starFall(star) {
+    var starTopStart = parseInt($(star).css("top").slice(0,-2));
+    var starLeftStart = parseInt($(star).css("left").slice(0,-2));
+    $(star).animate({
+        "opacity": "1",
+        "top": (400+starTopStart).toString()+"px",
+        "left": (400+starLeftStart).toString()+"px"
+    }, 2000, "linear", function() {
+        $(star).animate({
+            "opacity":"0"
+        },200, function() {
+            $(star).css("top", Math.floor(Math.random()*150).toString()+"px")
+            $(star).css("left",Math.floor(Math.random()*800).toString()+"px")
+        });
+    });
+}
