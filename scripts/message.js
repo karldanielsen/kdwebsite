b7300958eab715858d194edd7eb20fc324144b73
karldanielsen <karlpantry@gmail.com>
2021-04-05 19:58:37 -0700

    FIX final commit before CORS debugging

--- scripts/message.js
+++ scripts/message.js
@@ -1,7 +1,6 @@
 async function message() {
     var msg = $("#snsTextArea").val();
-    console.log(msg)
-    const response = await fetch('https://b8j38l4m27.execute-api.us-east-1.amazonaws.com/basic/',
+    const response = await fetch(`https://b8j38l4m27.execute-api.us-east-1.amazonaws.com/message`,
                                  {
                                      method: 'POST',
                                      mode: 'cors',
