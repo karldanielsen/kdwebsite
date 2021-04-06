f1d104777ed2467af9c87708843510731c67da81
karldanielsen <karlpantry@gmail.com>
2021-04-05 19:34:34 -0700

    FIX typo in scripts/message.js

--- scripts/message.js
+++ scripts/message.js
@@ -5,7 +5,8 @@ $(document).ready(function() {
 });
 
 async function message() {
-    var msg = $("#snsTestArea").val();
+    var msg = $("#snsTextArea").val();
+    console.log(msg)
     const response = await fetch('https://b8j38l4m27.execute-api.us-east-1.amazonaws.com/basic/',
                                  {
                                      method: 'POST',
